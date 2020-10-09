1476d331c031e3b5b6ff2a5f30817164ca12114b
andrea <andrea.czar@certn.co>
2020-10-08 17:49:42 -0700

    add comment

--- blog/views.py
+++ blog/views.py
@@ -28,6 +28,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
+    # This is a random comment
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
